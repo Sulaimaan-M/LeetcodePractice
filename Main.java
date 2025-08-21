e827432d3a2647caa751539889cfb718b613e142
SulaimaanM <sulaimaanm97@gmail.com>
2025-08-21 09:44:40 +0530

    Solved problem 1400

--- Main.java
+++ Main.java
@@ -1,31 +1,27 @@
 package leetcode;
 
-import java.util.Arrays;
-
-import leetcode.unsolved.problem_2966_divide_array.DivideArray;
+import leetcode.unsolved.problem_1400_can_construct_palindrome.CanConstruct;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        DivideArray obj = new DivideArray();
+        CanConstruct obj = new CanConstruct();
 
-        // Sample Arguements
+        // Sample Arguments
 
-        //[1,3,4,8,7,9,3,5,1], 2
+        //"annabelle", 2
 
-        //[2,4,2,2,5,2], 2
+        //"leetcode", 3
 
-        //[4,2,9,8,2,12,7,12,10,5,8,5,5,7,9,2,5,11], 14
-
+        // "true", 4
+        
         System.out.println(
-            Arrays.toString(
-                obj.divideArray(
+            obj.canConstruct(
 
-                // Add Test Arguments below
-                    new int[]{1,3,4,8,7,9,3,5,1}, 2
-                    
-                )
+            // Add Test Arguments below
+                "true", 4
+
             )
         );
        
--- unsolved/problem_1400_can_construct_palindrome/CanConstruct.java
+++ unsolved/problem_1400_can_construct_palindrome/CanConstruct.java
@@ -1,10 +1,39 @@
 package leetcode.unsolved.problem_1400_can_construct_palindrome;
 
+import java.util.HashSet;
+
 public class CanConstruct {
     
     public boolean canConstruct(String s, int k){
 
-        return true;
+        HashSet<Character> charSet = new HashSet<>();
+
+        if(s.length()<k){
+
+            return false;
+
+        }
+
+        for(int i = 0; i< s.length(); i++){
+
+            Character c = s.charAt(i);
+
+            if(charSet.contains(c)){
+
+                charSet.remove(c);
+
+            }else{
+
+                charSet.add(c);
+
+            }
+
+        }
+
+        if(charSet.size()<=k)
+            return true;
+        else
+            return false;
 
     }
 }
