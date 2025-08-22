f6ebb4e766b25c767e21c99db0adcff3bb706352
SulaimaanM <sulaimaanm97@gmail.com>
2025-08-22 18:30:48 +0530

    Solved 1052

--- Main.java
+++ Main.java
@@ -1,28 +1,30 @@
 package leetcode;
 
-import leetcode.problem_1400_can_construct_palindrome.CanConstruct;
+import leetcode.unsolved_or_solving.problem_1052_grumpy_owner.MaximumCustomers;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        CanConstruct obj = new CanConstruct();
-
-        // Sample Arguments
-
-        //"annabelle", 2
-
-        //"leetcode", 3
-
-        // "true", 4
-        
         System.out.println(
-            obj.canConstruct(
 
-            // Add Test Arguments below
-                "true", 4
+            /* Sample Arguments
+            
+            [1,0,1,2,1,1,7,5], [0,1,0,1,0,1,0,1], 3
 
-            )
+            [1], [0], 1
+
+            */ 
+
+            (new MaximumCustomers()).maxSatisfied(
+
+                // Test Argumemts Here
+
+                new int[]{4,10,10},
+                new int[]{1,1,0}, 
+                2)
+
+
         );
        
     }
--- unsolved_or_solving/problem_1052_grumpy_owner/MaximumCustomers.java
+++ unsolved_or_solving/problem_1052_grumpy_owner/MaximumCustomers.java
@@ -2,4 +2,39 @@ package leetcode.unsolved_or_solving.problem_1052_grumpy_owner;
 
 public class MaximumCustomers {
 
+    public int maxSatisfied(int[] customers, int[] grumpy, int minutes){
+
+        int res = 0, total = 0, highSec = 0 ;
+
+        for(int i = 0; i< minutes; i++){
+
+            total += (grumpy[i])*customers[i];
+
+        }
+
+        highSec = total;
+
+
+        
+        for(int i = minutes; i < customers.length; i++){
+
+            //int to = i-minutes;
+            total += grumpy[i]*customers[i];
+            total -= grumpy[i-minutes]*customers[i-minutes];
+            highSec = Math.max(total, highSec);
+
+        }
+
+        for(int i = 0; i< customers.length; i++){
+
+            res += (grumpy[i] == 0? 1 : 0)*customers[i];
+
+        }
+
+        res+=highSec;
+
+        return res;
+
+    }
+
 }
