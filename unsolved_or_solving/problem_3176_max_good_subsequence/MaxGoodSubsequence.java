4f06ffcb74fbf90d7e0f8c30856b423adddb9cd3
SulaimaanM <sulaimaanm97@gmail.com>
2025-08-26 16:37:25 +0530

    Added 3176

---
+++ unsolved_or_solving/problem_3176_max_good_subsequence/MaxGoodSubsequence.java
@@ -0,0 +1,13 @@
+package leetcode.unsolved_or_solving.problem_3176_max_good_subsequence;
+
+public class MaxGoodSubsequence {
+
+    public int maxGoodSubsequence(int[] nums, int k){
+
+        int res = 0;
+
+        return res;
+
+    }
+    
+}
