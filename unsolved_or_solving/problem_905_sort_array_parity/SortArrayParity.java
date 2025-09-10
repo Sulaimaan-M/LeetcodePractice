c808afe71666b400472ad6e2996cd318c3ad8c59
SulaimaanM <sulaimaanm97@gmail.com>
2025-09-10 10:47:09 +0530

    Solved Problem 905

---
+++ unsolved_or_solving/problem_905_sort_array_parity/SortArrayParity.java
@@ -0,0 +1,33 @@
+package leetcode.unsolved_or_solving.problem_905_sort_array_parity;
+
+public class SortArrayParity {
+
+    public int[] sortArrayParity(int[] nums){
+
+        int length = nums.length;
+        int[] sorted = new int[length];
+        int pointerA = 0, pointerB = length-1;
+
+        for( int i = 0; i< length ; i++){
+
+            int value = nums[i];
+
+            if(value%2 ==0){
+
+                sorted[pointerA] = value;
+                pointerA++;
+
+            }else{
+
+                sorted[pointerB] = value;
+                pointerB--;
+
+            }
+
+        }
+
+        return sorted;
+
+    }
+
+}
