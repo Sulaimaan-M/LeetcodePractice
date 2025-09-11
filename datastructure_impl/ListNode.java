b0c99561c372d0e6882e247062f8e6780df4931d
SulaimaanM <sulaimaanm97@gmail.com>
2025-09-11 08:39:28 +0530

    Altered to work with my code

--- datastructure_impl/ListNode.java
+++ datastructure_impl/ListNode.java
@@ -6,11 +6,11 @@ public class ListNode {
     ListNode next;
     ListNode() {}
 
-    ListNode(int val) { 
+    public ListNode(int val) { 
         this.val = val; 
     }
     
-    ListNode(int val, ListNode next) { 
+    public ListNode(int val, ListNode next) { 
         this.val = val; this.next = next; 
     }
 
