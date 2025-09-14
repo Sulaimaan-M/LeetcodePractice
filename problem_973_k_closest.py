10c96d163682b98295c440a35a089c8b14b0e641
SulaimaanM <sulaimaanm97@gmail.com>
2025-09-14 09:26:31 +0530

    Some Formatting

--- problem_973_k_closest.py
+++ problem_973_k_closest.py
@@ -1,19 +1,20 @@
 import heapq
-def kClosest( points: list[list[int]], k: int) -> list[list[int]]:
 
-        minHeap = []
+def kClosest(points: list[list[int]], k: int) -> list[list[int]]:
 
-        for x, y in points:
-            distance = (x ** 2)+(y ** 2)
-            minHeap.append([distance, x, y])
-        
-        heapq.heapify(minHeap)
-        res = []
+    minHeap = []
 
-        while k > 0:
+    for x, y in points:
+        distance = (x**2) + (y**2)
+        minHeap.append([distance, x, y])
 
-            dist, x, y = heapq.heappop(minHeap)
-            res.append([x,y])
-            k = k-1
-        
-        return res
+    heapq.heapify(minHeap)
+    res = []
+
+    while k > 0:
+
+        dist, x, y = heapq.heappop(minHeap)
+        res.append([x, y])
+        k = k - 1
+
+    return res
