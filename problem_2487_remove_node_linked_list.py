3f88b882f3257bc1709961d60dd2cd52d0e33e0c
SulaimaanM <sulaimaanm97@gmail.com>
2025-09-17 17:43:34 +0530

    Solved Problem 2487

---
+++ problem_2487_remove_node_linked_list.py
@@ -0,0 +1,36 @@
+from linked_list import ListNode
+from typing import Optional
+import collections
+
+
+def removeNodes(head: Optional[ListNode]) -> Optional[ListNode]:
+
+    nodeStack = collections.deque()
+
+    currentNode = head
+
+    while currentNode:
+
+        nodeStack.append(currentNode)
+
+        currentNode = currentNode.next
+
+    highestNode = None
+
+    while len(nodeStack) > 0:
+
+        a = nodeStack.pop()
+
+        a.next = None
+
+        if highestNode is None:
+
+            highestNode = a
+
+        elif a.val >= highestNode.val:
+
+            a.next = highestNode
+
+            highestNode = a
+
+    return highestNode
